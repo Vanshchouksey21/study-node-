4d88b7d40e8dcfa1f251e0aaa6ce7465f55d285d
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2026-02-23 23:02:30 +0530

    basics od crud done

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const connectdb = require("./utils/db");
 const UserModel = require("./models/userModel");
 const app = express();
 
+app.use(express.json());
 
+// signup
 app.post("/signup" , async (req , res) =>{
-try {
-        const dummyuser = {
-        firstName:"vansh",
-        lastName:"chouksey",
-        emailId:"vansh@gmail.com",
-        password:"1234567",
-        mobileNumber:"12345678"
-    }
+try{ 
 
-    const user  = new UserModel(dummyuser);
+    const user  = new UserModel(req.body);
     await user.save();
     res.status(200).send("useer added succesfullly");
 } catch (error) {
@@ -22,8 +17,68 @@ try {
     res.status(400).send("error" , error.message)
     
 }
+
+console.log(req);
+
 })
 
+//login
+app.get("/getuser" , async(req , res) =>{
+    const userEmail = req.body.emailId ;
+
+try {
+        const getuserdetails = await UserModel.findOne({emailId:userEmail})
+    console.log(getuserdetails);
+    if(getuserdetails.length === 0){
+        return res.status(404).send("user not found ");
+    }
+    res.status(200).send(getuserdetails)
+} catch (error) {
+    console.error(error.message);
+    res.status(400).send("something went wrong")
+    
+}
+    
+})
+
+
+// delete
+
+app.delete("/deleteuser" , async(req ,res) =>{
+    const userid = req.body._id;
+    try {
+        const deleteUser = await UserModel.findByIdAndDelete({_id : userid});
+        console.log(deleteUser);
+        
+        res.send(deleteUser);
+
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
+
+app.patch("/updateuser" , async(req , res) =>{
+    const updatedDeatils = req.body ; 
+    console.log(updatedDeatils);
+    
+    const userId = req.body._id ; 
+    try {
+        const updateduser = await UserModel.findByIdAndUpdate({_id:userId} , {updatedDeatils});
+        console.log(updateduser);
+        console.log("user updated sucessfulyy");
+        res.status(200).send(updateduser);
+        
+        
+        
+    } catch (error) {
+
+       console.error(error.message);
+       res.status(400).send("something went wrong")
+        
+    }
+})
 // wildcard error handling
 
 app.use("/" , (err , req , res, next)=>{
